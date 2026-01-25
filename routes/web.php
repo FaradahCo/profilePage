5880c2afde1ff13fdb4f8242d41cb6ec198c01c7
x-JOC <oshalfan@faradah.sa>
2026-01-25 17:15:38 +0300

    feat: Add direct view and its corresponding web route.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// الصفحة الرئيسية - روابط مباشرة
 Route::get('/', function () {
+    return view('direct');
+});
+
+// صفحة iframe
+Route::get('/i', function () {
     return view('welcome');
 });
