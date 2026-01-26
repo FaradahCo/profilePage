27a60370a227de288dc1e40b7f22e20da857baeb
x-JOC <oshalfan@faradah.sa>
2026-01-26 11:24:32 +0300

    ب

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -525,14 +525,72 @@
             });
         });
 
-        // Form Submission Handler
+        // Device Detection Helper Functions
+        function getDeviceType() {
+            const ua = navigator.userAgent;
+            if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+                return 'Tablet';
+            }
+            if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+                return 'Mobile';
+            }
+            return 'Desktop';
+        }
+
+        function getBrowserInfo() {
+            const ua = navigator.userAgent;
+            let browserName = 'Unknown';
+            let browserVersion = 'Unknown';
+
+            if (ua.indexOf('Firefox') > -1) {
+                browserName = 'Firefox';
+                browserVersion = ua.match(/Firefox\/([0-9.]+)/)?.[1] || 'Unknown';
+            } else if (ua.indexOf('SamsungBrowser') > -1) {
+                browserName = 'Samsung Browser';
+                browserVersion = ua.match(/SamsungBrowser\/([0-9.]+)/)?.[1] || 'Unknown';
+            } else if (ua.indexOf('Opera') > -1 || ua.indexOf('OPR') > -1) {
+                browserName = 'Opera';
+                browserVersion = ua.match(/(?:Opera|OPR)\/([0-9.]+)/)?.[1] || 'Unknown';
+            } else if (ua.indexOf('Trident') > -1) {
+                browserName = 'Internet Explorer';
+                browserVersion = ua.match(/rv:([0-9.]+)/)?.[1] || 'Unknown';
+            } else if (ua.indexOf('Edge') > -1) {
+                browserName = 'Edge (Legacy)';
+                browserVersion = ua.match(/Edge\/([0-9.]+)/)?.[1] || 'Unknown';
+            } else if (ua.indexOf('Edg') > -1) {
+                browserName = 'Edge (Chromium)';
+                browserVersion = ua.match(/Edg\/([0-9.]+)/)?.[1] || 'Unknown';
+            } else if (ua.indexOf('Chrome') > -1) {
+                browserName = 'Chrome';
+                browserVersion = ua.match(/Chrome\/([0-9.]+)/)?.[1] || 'Unknown';
+            } else if (ua.indexOf('Safari') > -1) {
+                browserName = 'Safari';
+                browserVersion = ua.match(/Version\/([0-9.]+)/)?.[1] || 'Unknown';
+            }
+
+            return `${browserName} ${browserVersion}`;
+        }
+
+        function getOSInfo() {
+            const ua = navigator.userAgent;
+            let os = 'Unknown';
+
+            if (ua.indexOf('Win') > -1) os = 'Windows';
+            else if (ua.indexOf('Mac') > -1) os = 'macOS';
+            else if (ua.indexOf('Linux') > -1) os = 'Linux';
+            else if (ua.indexOf('Android') > -1) os = 'Android';
+            else if (ua.indexOf('like Mac') > -1) os = 'iOS';
+
+            return os;
+        }
+
         // Form Submission Handler
         function handleFormSubmit(e) {
             e.preventDefault();
 
             // <--- ضع رابط السكربت الخاص بك هنا بعد النشر الجديد
             // LINK_PLACEHOLDER
-            const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyEsstUDsyRgKcBpMg4HeiTNkDsiLenFjb39obGjgTK330_pdohXqB22j8xJDBMuNOT-w/exec';
+            const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyW4TRrsHIlatrWQZcFX9tFNuq7IGlobXz9lxGubmjs_6FBESAu8enPgr9yDSl9rKQZhA/exec';
 
             if (SCRIPT_URL.includes('YOUR_GOOGLE_APPS_SCRIPT')) {
                 alert('الرجاء إعداد رابط Google Apps Script أولاً في الكود.');
@@ -555,6 +613,17 @@
             // Handle array for platforms
             const platforms = formData.getAll('platforms[]');
             platforms.forEach(p => data.append('platforms[]', p));
+
+            // Add device and tracking information
+            data.append('page_type', 'شاشة'); // Iframe page = Screen
+            data.append('device_type', getDeviceType());
+            data.append('browser', getBrowserInfo());
+            data.append('os', getOSInfo());
+            data.append('screen_size', `${window.screen.width}x${window.screen.height}`);
+            data.append('viewport_size', `${window.innerWidth}x${window.innerHeight}`);
+            data.append('timestamp', new Date().toISOString());
+            data.append('timezone', Intl.DateTimeFormat().resolvedOptions().timeZone);
+            data.append('language', navigator.language);
 
             // 1. Show success Toast immediately
             showToast('شكراً لتسجيل اهتمامك! سنتواصل معك قريباً.');
